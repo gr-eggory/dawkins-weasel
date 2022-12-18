dc399c1008c334230ac21e04dbf98098bcee22f7
gr-eggory <greggroks@gmail.com>
2022-12-18 14:45:00 -0500

    Organizing output with grid[p

--- src/App.svelte
+++ src/App.svelte
@@ -31,15 +31,33 @@
 </script>
 
 <main>
-	<h2>{optimal}</h2>
-	<h2>{closestIndividual}</h2>
-	<h3>Generation: {generation}</h3>
+	<div class="output-grid">
+		<h3>Optimal:</h3>
+		<h2>{optimal}</h2>
+		<h3>Most successful child:</h3>
+		<h2>{closestIndividual}</h2>
+		<h3 class="generation">Generation: {generation}</h3>
+	</div>
 	<input type="text" bind:value={input} on:submit={assignOptimal} />
+	<button>Begin Simulation</button>
 </main>
 
 <style>
 	main {
 		min-height: 100vh;
 		background-color: whitesmoke;
+		padding-top: 5rem;
+	}
+
+	.output-grid {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		width: 70%;
+		margin: 0 auto;
+		gap: 1rem;
+	}
+
+	.generation {
+		grid-column: 2;
 	}
 </style>
